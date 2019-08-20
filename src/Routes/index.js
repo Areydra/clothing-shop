53ffe9b655e4d1ff45b711817004e01c0d5d3306
Areydra <areydra@gmail.com>
2019-08-20 20:00:57 +0700

    add type to mvc concept

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const branchs = require('./branchs')
-const router = express.Router()
+const types   = require('./types')
+const router  = express.Router()
 
 
 
 router.use('/branchs', branchs) //menggunakan endpoint/router tambahan dari router branchs
+router.use('/types', types)
 
 module.exports = router
--- src/Routes/types.js
+++ src/Routes/types.js
@@ -1,29 +1,11 @@
-module.exports = (app, db, formResponse) => {
-    
-    //=============== ROUTE CRUD TYPES ================
-    app.get('/types', (req, res) => {
-        db.query('SELECT * FROM types', (error, response) => {
-            (error) ? console.log(error): res.json(formResponse(200, response))
-        })
-    })
-    app.get('/type/:id', (req, res) => {
-        db.query('SELECT * FROM types WHERE id=?', [req.params.id], (error, response) => {
-            (error) ? console.log(error): res.json(formResponse(200, response))
-        })
-    })
-    app.post('/type', (req, res) => {
-        db.query('INSERT INTO types (name) VALUES (?)', [req.body.name], (error, response) => {
-            (error) ? console.log(error): res.json(formResponse(200, response))
-        })
-    })
-    app.patch('/type/:id/update', (req, res) => {
-        db.query('UPDATE types SET name=? WHERE id=?', [req.body.name, req.params.id], (error, response) => {
-            (error) ? console.log(error): res.json(formResponse(200, response))
-        })
-    })
-    app.delete('/type/:id/delete', (req, res) => {
-        db.query('DELETE FROM types WHERE id=?', [req.params.id], (error, response) => {
-            (error) ? console.log(error): res.json(formResponse(200, response))
-        })
-    })
-}
+const express = require('express')
+const router  = express.Router()
+const types   = require('../Controllers/types')
+
+router.get('/', types.getTypes)
+router.get('/:id', types.getTypeById)
+router.post('/', types.postType)
+router.patch('/:id/update', types.patchType)
+router.delete('/:id/delete', types.deleteType)
+
+module.exports = router
